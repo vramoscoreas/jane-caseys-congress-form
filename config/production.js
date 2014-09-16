74109a27a0cf56be611f1b5b4f90c876494a756d
Kevin Thayer <kthayer@ngpvan.com>
2014-09-16 11:36:40 -0400

    Added jshint and congress form url

--- config/production.js
+++ config/production.js
@@ -4,6 +4,7 @@ module.exports = {
   app: {
     port: port,
     pub_dir: '/public',
-    cookie_secret: 'Your secret key salt goes here. Replace with random, long string in your project.'
+    cookie_secret: 'Your secret key salt goes here. Replace with random, ' +
+      'long string in your project.'
   }
 };
